62954f65e4232f775f9d2e64ab2794d56d970342
Dayne Wright <daynewr@gmail.com>
2016-09-01 23:51:09 -0500

    added routing

--- app/app.js
+++ app/app.js
@@ -1,3 +1,13 @@
 "use strict";
 
-let app = angular.module('TravelApp', []);
+let app = angular.module('TravelApp', ['ngRoute']);
+
+app.config(['$routeProvider', function($routeProvider){
+  $routeProvider
+  .when('/', {
+    templateUrl: 'partials/guide-list.html',
+    controller: 'BookCtrl'
+  })
+  .otherwise('/');
+
+}]);
